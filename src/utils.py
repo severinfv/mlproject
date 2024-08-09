d06996a1b729bb98d2fe1279dc9ba5b91ed74e16
Severin F <severin.filonov@pm.me>
2024-08-09 15:27:04 +0200

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -33,9 +33,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
+            model.fit(X_train,y_train)  #Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -48,6 +46,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
