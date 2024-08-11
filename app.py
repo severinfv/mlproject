519bb88cfbe1bc00104bca0bdd20fd21f3d15a5c
Severin F <severin.filonov@pm.me>
2024-08-11 11:44:02 +0200

    update

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def predict_datapoint():
             writing_score=float(request.form.get('reading_score')))
         
         pred_df=data.get_data_as_data_frame()
-        print(pred_df)
+       # print(pred_df)
 
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
